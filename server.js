2df2d2275478102c648588682985893c2d8b6859
Ranjith Babu <ranjithbabu0912@gmail.com>
2024-11-26 12:58:22 +0530

    Set up a basic Node.js server

---
+++ server.js
@@ -0,0 +1,22 @@
+// Import the Express library
+const express = require('express');
+
+// Create an instance of an Express application
+const app = express();
+
+// Middleware to serve static files from the "public" folder
+app.use(express.static('public'));
+
+// Serve the index.html file for the root route
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+// Define the port for the server to listen on
+const PORT = 3000;
+
+// Start the server and log a message to the console
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
